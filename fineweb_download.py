96325b3874eab0990b63472bdad7300925c4212f
CharlesJonah <mutua.charles48@gmail.com>
2025-09-01 00:25:39 +0300

    dataset downloading

---
+++ fineweb_download.py
@@ -0,0 +1,79 @@
+''' 
+FineWeb-Edu dataset (for srs pretraining)
+https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu
+Downloads and tokenizes the data and saves data shards to disk.
+Run simply as: python fineweb_download.py
+Will save shards to the local directory "edu_fineweb10B".
+'''
+
+
+import os
+import multiprocessing as mp
+
+import tiktoken
+from tqdm import tqdm
+from datasets import load_dataset
+import numpy as np
+
+# Fineweb dataset downloading consts
+DATA_CACHE_DIR = os.path.join(os.path.dirname(__name__), "edu-fineweb10B")
+SHARD_SIZE = int(1e8) # 100M tokens per shard, total 100 shards
+
+
+
+def download_data():
+    return load_dataset("HuggingFaceFW/fineweb", name="sample=10BT", split='train')
+
+@staticmethod
+def tokenize(doc):
+    # init the tokenizer
+    enc = tiktoken.get_encoding('gpt2')
+    eot = enc._special_tokens['<|endoftext|>'] # end of text token
+    tokens = [eot]
+    tokens.extend(enc.encode_ordinary(doc['text']))
+    tokens_np = np.array(tokens)
+    assert (0 <= tokens_np).all() and (tokens_np <= 2**16).all(), "token dictionary too large for uint16"
+    tokens_np_uint16 = tokens_np.astype(np.uint16)
+    return tokens_np_uint16
+    
+
+def write_datafile(filename, tokens_np):
+    with open(filename, "wb") as f:
+        f.write(tokens_np.tobytes())
+            
+def orcherstrate_dataset_downloading():
+    os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+    
+    # tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
+    nprocs = max(1, os.cpu_count()//2)
+    with mp.Pool(nprocs) as pool:
+        shard_index = 0
+        # preallocate buffer to hold current shard
+        all_tokens_np = np.empty((SHARD_SIZE,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = None
+        for tokens in pool.imap(tokenize, download_data(), chunksize=16):
+            # is there enough space in the current shard for the new tokens?
+            if token_count + len(tokens) < SHARD_SIZE:
+                # simply append tokens to current shard
+                all_tokens_np[token_count:token_count+len(tokens)] = tokens
+                token_count += len(tokens)
+                # update progress bar
+                if progress_bar is None:
+                    progress_bar = tqdm(total=SHARD_SIZE, unit="tokens", desc=f"Shard {shard_index}")
+                progress_bar.update(len(tokens))
+            else:
+                # write the current shard and start a new one
+                split = "val" if shard_index == 0 else "train"
+                filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}")
+                # split the document into whatever fits in this shard; the remainder goes to next one
+                remainder = SHARD_SIZE - token_count
+                progress_bar.update(remainder)
+                all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
+                write_datafile(filename, all_tokens_np)
+                shard_index += 1
+                progress_bar = None
+                # populate the next shard with the leftovers of the current doc
+                all_tokens_np[0:len(tokens)-remainder] = tokens[remainder:]
+                token_count = len(tokens)-remainder
+        
